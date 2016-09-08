882dc52b2cbcfb4fd5ec5523636ad2216fb94ba8
elleivan <elleivan@greeny.cs.tlu.ee>
2016-09-08 13:11:10 +0300

    added my first lines of codes

---
+++ first_file.php
@@ -0,0 +1,15 @@
+<?php
+
+	$firstName = "Elle";
+	$lastName = "Ivantsikova";
+	
+	
+	// echo $firstName;
+	// echo $lastName;
+
+	// Kahe stringi liitmiseks kasutan punkti
+	// echo voimaldab valja trukkimist
+	echo $firstName." ".$lastName;
+	
+?>
+
